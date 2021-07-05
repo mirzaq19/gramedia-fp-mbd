fe9fa5916c6de50ad18c712f0359155fa634b5bf
mirzaq19 <mirzaqarjap@gmail.com>
2021-07-06 05:49:21 +0700

    Tambah trigger

--- app/Models/TransaksiDetail.php
+++ app/Models/TransaksiDetail.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Dipesan extends Model
+class TransaksiDetail extends Model
 {
     use HasFactory;
 }
--- database/seeders/TransaksiDetailSeeder.php
+++ database/seeders/TransaksiDetailSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DipesanSeeder extends Seeder
+class TransaksiDetailSeeder extends Seeder
 {
     /**
      * Run the database seeds.
