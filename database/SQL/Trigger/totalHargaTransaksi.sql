9deb88ca3d2f9ba42b372a686f3ce6976462ea88
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-07-09 00:52:54 +0700

    updated trigger totalhargatransaksi

--- database/SQL/Trigger/totalHargaTransaksi.sql
+++ database/SQL/Trigger/totalHargaTransaksi.sql
@@ -1,18 +1,26 @@
 CREATE OR REPLACE FUNCTION totalHargaTransaksi() RETURNS trigger AS $totalHargaTransaksi$
     DECLARE
         total_transaksi DECIMAL(10,2);
+		total_berat DECIMAL(10,2);
 
     BEGIN
         SELECT SUM(subtotal) INTO total_transaksi FROM transaksi_detail 
-            WHERE transaksi_detail.id_transaksi = transaksi.id_transaksi;
+            WHERE id_transaksi = new.id_transaksi;
 
-        transaksi.jumlah_biaya = total_transaksi;
+        SELECT SUM(produk.berat)*SUM(transaksi_detail.jumlah) INTO total_berat FROM transaksi_detail
+            INNER JOIN produk
+            ON produk.id_produk = new.produk AND transaksi_detail.id_transaksi = new.id_transaksi;
+		
+		UPDATE transaksi
+        SET jumlah_biaya = total_transaksi + ((SELECT biaya_perkg FROM jasa_kirim
+                                                WHERE id_jasakirim = (SELECT id_jasakirim FROM transaksi
+																	 	WHERE id_transaksi = new.id_transaksi)) * (total_berat/2000))
+		WHERE id_transaksi = new.id_transaksi;
         
         RETURN new;
     END
 
 $totalHargaTransaksi$ LANGUAGE plpgsql;
 
-CREATE TRIGGER totalHargaTransaksi BEFORE INSERT OR UPDATE ON transaksi_detail
+CREATE TRIGGER totalHargaTransaksi AFTER INSERT OR UPDATE ON transaksi_detail
 FOR EACH ROW EXECUTE FUNCTION totalHargaTransaksi();
-
