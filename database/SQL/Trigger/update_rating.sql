8a68c050ee40c72b2ee3383ca24979e977f7f6ef
mirzaq19 <mirzaqarjap@gmail.com>
2021-07-08 23:41:20 +0700

    update trigger rating

--- database/SQL/Trigger/update_rating.sql
+++ database/SQL/Trigger/update_rating.sql
@@ -2,23 +2,33 @@
 CREATE OR REPLACE FUNCTION update_rating() RETURNS trigger AS $update_rating$
 	DECLARE X INT;
     BEGIN
-		SELECT COUNT(*) INTO X FROM testimoni WHERE id_produk = NEW.id_produk;
-		
-		IF X = 0 THEN
-			UPDATE produk SET rating = NEW.rating WHERE id_produk = NEW.id_produk;
+		IF TG_OP = 'DELETE' THEN
+			SELECT COUNT(id_testimoni) INTO X FROM testimoni WHERE id_produk = OLD.id_produk
+
+			IF X = 0 THEN
+				UPDATE produk SET rating = 0 WHERE id_produk = OLD.id_produk;
+			END IF;
+
+			IF X <> 0 THEN			
+				UPDATE produk SET rating = (SELECT AVG(rating) 
+											FROM testimoni 
+											WHERE id_produk = OLD.id_produk)
+				WHERE id_produk = OLD.id_produk;
+			END IF;
+
+			RETURN NEW;
 		END IF;
-		
-		IF X <> 0 THEN
-			UPDATE produk SET rating = (SELECT AVG(rating) 
-										FROM testimoni 
-										WHERE id_produk = NEW.id_produk)
-			WHERE id_produk = NEW.id_produk;
-		END IF;
-		
+
+		UPDATE produk SET rating = (SELECT AVG(rating) 
+									FROM testimoni 
+									WHERE id_produk = NEW.id_produk)
+		WHERE id_produk = NEW.id_produk;
+
 		RETURN NEW;
 	END;
 $update_rating$ LANGUAGE plpgsql;
 
 -- Trigger
-CREATE TRIGGER update_rating AFTER INSERT OR UPDATE ON testimoni
+CREATE TRIGGER update_rating AFTER INSERT OR UPDATE OR DELETE ON testimoni
 FOR EACH ROW EXECUTE FUNCTION update_rating();
+
