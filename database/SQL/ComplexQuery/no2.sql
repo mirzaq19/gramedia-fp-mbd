f25630cddae275e2ee9d503c3ba25235a1ebee90
ahdan86 <ahdanamanullah@gmail.com>
2021-07-10 00:57:29 +0700

    Update no2.sql

--- database/SQL/ComplexQuery/no2.sql
+++ database/SQL/ComplexQuery/no2.sql
@@ -1,4 +1,5 @@
 SELECT nama, SUM(transaksi.jumlah_biaya) AS "Jumlah Pembelian" 
 FROM akun 
 JOIN transaksi ON transaksi.id_akun = akun.id_akun
-GROUP BY akun.nama; 
+GROUP BY akun.nama
+ORDER BY SUM(transaksi.jumlah_biaya) DESC;
