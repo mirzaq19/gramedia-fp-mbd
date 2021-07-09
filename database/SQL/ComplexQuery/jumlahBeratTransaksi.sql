21cea02835174ffdbf405ea29e005f2b97c2345f
mirzaq19 <mirzaqarjap@gmail.com>
2021-07-09 15:00:21 +0700

    Revisi trigger

--- database/SQL/ComplexQuery/jumlahBeratTransaksi.sql
+++ database/SQL/ComplexQuery/jumlahBeratTransaksi.sql
@@ -1,5 +1,5 @@
---Complex Query: Mencari total berat pada tiap transaksi
-SELECT (SUM(produk.berat*transaksi_detail.jumlah)::DECIMAL/1000)::DECIMAL(10,2) AS "Total Berat"
+SELECT (SUM(produk.berat*transaksi_detail.jumlah)::DECIMAL/1000)::DECIMAL(10,2)*
+(SELECT biaya_perkg FROM jasa_kirim WHERE id_jasakirim=1) AS "Biaya Jasa Kirim"
 FROM produk JOIN 
 transaksi_detail ON transaksi_detail.id_produk = produk.id_produk
 WHERE transaksi_detail.id_transaksi = 1;
